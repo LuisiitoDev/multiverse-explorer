386ffeeb338fa7a7765a763c537363d1d29e2d98
LuisiitoDev <luisasandovalcoder@gmail.com>
2026-08-04 20:11:41 -0600

    feat: add CI workflow for build and deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  base: '/react-github-actions-demo/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
